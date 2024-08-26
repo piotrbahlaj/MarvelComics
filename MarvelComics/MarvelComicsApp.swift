677b4edb609ee3c866f9d580e41a8437deda139f
Piotr Bahlaj <piotr.bahlaj@o2.pl>
2024-08-26 11:49:36 +0200

    Initial Commit

---
+++ MarvelComics/MarvelComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelComicsApp.swift
+//  MarvelComics
+//
+//  Created by Piotr Bahlaj on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarvelComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
